aae2532c7b15eb4afcbc367140947f4aa37f4292
pgsil <email@email.com>
2018-03-20 21:38:35 -0300

    set content-type for css serve

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const randStr = require('randomstring').generate;
+const fs = require("fs");
+const randStr = require("randomstring").generate;
 
-const jsonDatabase = require('./data');
+const jsonDatabase = require("./data");
 
 const getStylesFromUrl = (styleString, database) => {
-    const styles = styleString.split(',');
+  const styles = styleString.split(",");
 
-    let hasAllStyles = true;
+  let hasAllStyles = true;
 
+  styles.forEach(element => {
+    if (!(element in database)) {
+      hasAllStyles = false;
+      console.log(element + " not found");
+    }
+  });
+
+  let styleCode = "";
+
+  if (hasAllStyles) {
     styles.forEach(element => {
-        if (!(element in database)) {
-            hasAllStyles = false;
-            console.log(element + ' not found');
-        }
+      styleCode = styleCode + database[element].code;
     });
 
-    let styleCode = '';
+    return styleCode;
+  } else {
+    throw new Error("Bad input. One or more styles don't exist");
+  }
+};
 
-    if (hasAllStyles) {
-        styles.forEach(element => {
-            styleCode = styleCode + database[element].code
-        });
+const writeNewStyle = cssString => {
+  const filePath = randStr(6) + ".css";
 
-        return styleCode
-    }
-    else {
-        throw new Error('Bad input. One or more styles don\'t exist')
-    }
-}
+  fs.writeFileSync("./cached/" + filePath, cssString, err => {
+    throw new Error(err);
+  });
 
-const writeNewStyle = (cssString) => {
-    const filePath = randStr(6) + '.css';
-
-    fs.writeFileSync('./cached/' + filePath, cssString, (err) => { throw new Error(err) });
-
-    return '/cached/' + filePath
-}
+  return "/cached/" + filePath;
+};
 
 const createCss = (styleString, database) => {
-    return writeNewStyle(getStylesFromUrl(styleString, database))
-}
+  return writeNewStyle(getStylesFromUrl(styleString, database));
+};
 
-app.get('/styles/:styles', (req, res) => {
-    if (req.params.styles.length > 0) {
-        res.send(req.protocol + '://' + req.get('host') + createCss(req.params.styles, jsonDatabase))
-    }
-    else {
-        res.sendStatus(500)
-    }
+app.get("/styles/:styles", (req, res) => {
+  if (req.params.styles.length > 0) {
+    res.send(req.protocol + "://" + req.get("host") + createCss(req.params.styles, jsonDatabase));
+  } else {
+    res.sendStatus(500);
+  }
 });
 
-app.get('/cached/:file', (req, res) => res.sendFile(__dirname + '/cached/' + req.params.file));
+app.get("/cached/:file", (req, res) => {
+  res.writeHead("Content-Type: text/css");
+  res.sendFile(__dirname + "/cached/" + req.params.file);
+});
 
-app.get('/list', (req, res) => res.sendFile(__dirname + '/data.json'));
+app.get("/list", (req, res) => res.sendFile(__dirname + "/data.json"));
 
-app.get('/*', (req, res) => res.sendStatus(404));
+app.get("/*", (req, res) => res.sendStatus(404));
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log("Example app listening on port 3000!"));
